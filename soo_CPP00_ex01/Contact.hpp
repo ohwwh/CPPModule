bc44189caffe7f59375a1942ff08722d4f05ad79
ohwwh <ohw1237@naver.com>
2022-11-18 21:07:50 +0900

    CPP 디버깅 launch.json 수정

---
+++ soo_CPP00_ex01/Contact.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soo <soo@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/12 16:30:45 by soo               #+#    #+#             */
+/*   Updated: 2022/11/18 16:54:34 by soo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <string>
+# include <iostream>
+# include <iomanip>
+
+class Contact
+{
+	private :
+		std::string contactFirstName;
+		std::string contactLastName;
+		std::string contactNickName;
+		std::string contactPhoneNumber;
+		std::string contactDarkestSecret;
+
+	public :
+		std::string		getContactFirstName();
+		void			setContactFirstName(std::string s);
+		std::string		getContactLastName();
+		void			setContactLastName(std::string s);
+		std::string		getContactNickName();
+		void			setContactNickName(std::string s);
+		std::string		getContactPhoneNumber();
+		void			setContactPhoneNumber(std::string s);
+		std::string		getContactDarkestSecret();
+		void			setContactDarkestSecret(std::string s);
+		
+		bool	checkInput();
+		std::string	getShortStr(std::string str);
+		std::string	inputInfo(std::string str);
+		void	addContact();
+		void	showContact();
+		void	showDetailContact();
+};
+
+#endif
---
+++ soo_CPP00_ex01/PhoneBook.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soo <soo@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/12 15:05:38 by soo               #+#    #+#             */
+/*   Updated: 2022/11/18 16:53:56 by soo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+#include "Contact.hpp"
+
+class PhoneBook
+{
+	private :
+		Contact contacts[8];
+		int currentIdx;
+		int	currentCnt;
+
+	public :
+		PhoneBook();
+		void	setCurrentIdx();
+		int		getCurrentIdx();
+		void	setCurrentCnt();
+		int		getCurrentCnt();
+
+		bool	contactIsEmpty();
+		bool	contactIsFull();
+
+		void	addNewContact();
+		void	searchContact();
+};
+
+#endif
